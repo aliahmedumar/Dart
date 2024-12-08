00870cbb426cb4ba75cec77d86d31d461e730989
aliahmedumar <aliahmedumar902@gmail.com>
2024-12-08 23:46:47 +0500

    Dart code

---
+++ today.dart
@@ -0,0 +1,8 @@
+void main() {
+  List studentName = [1, 2, 3, 4, 5, 6];
+  studentName.add(3);
+  print(studentName);
+  List student = [1, 2, 3, 4,5];
+  student.retainWhere((element) => element < 
+  print(student);
+}
