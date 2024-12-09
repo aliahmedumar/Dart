b703aefa80bb6c1613d18f72cf2bc4c1e8a73533
aliahmedumar <aliahmedumar902@gmail.com>
2024-12-09 09:17:36 +0500

    Dart file

--- today.dart
+++ today.dart
@@ -3,6 +3,6 @@ void main() {
   studentName.add(3);
   print(studentName);
   List student = [1, 2, 3, 4,5];
-  student.retainWhere((element) => element < 
+  student.retainWhere((element) => element < 3); 
   print(student);
 }
